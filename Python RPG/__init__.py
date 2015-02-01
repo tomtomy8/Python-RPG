bdb40a35f8ddb4af32d8e507830f98c51d5e261e
tomtomy8 <tommy.birt@gmail.com>
2015-02-01 11:43:40 +0000

    First commits
    Added in metadata.py, ___init___.py and LICENSE.md

---
+++ Python RPG/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""This is a targeted Python/Pygame development libary"""
+
+from Python_RPG import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ Python RPG/metadata.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'Python_RPG'
+project = "Python RPG Maker"
+project_no_spaces = project.replace(' ', '')
+version = '0.01'
+description = """
+This module has been written in Python and Pygame for a target game
+developer audince of RPG games, specificlly ones similar to JRPGs.
+"""
+authors = ['tomtomy8']
+authors_string = ', '.join(authors)
+emails = ['tommy.birt@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/tomtomy8/Python-RPG/'
